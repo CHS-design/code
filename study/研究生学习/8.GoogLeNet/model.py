388280e4fc616a3d523edeaf20dd395faf2bc77b
CHS-design <19951066185@163.com>
2026-07-03 18:58:53 +0800

    添加 GoogLeNet 模块实现，

--- study/研究生学习/8.GoogLeNet/model.py
+++ study/研究生学习/8.GoogLeNet/model.py
@@ -49,3 +49,63 @@ class Inception(nn.Module):
         #将四条路线的输出结果在通道维度上进行拼接
         outputs = torch.cat((out1, out2, out3, out4), dim=1)
         return outputs   
+    
+class GoogLeNet(nn.Module):
+    def __init__(self,Inception):
+        super(GoogLeNet, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=7, stride=2, padding=3),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=64, out_channels=192, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        )
+        self.block3 = nn.Sequential(
+            Inception(192, 64, (96, 128), (16, 32), 32),
+            Inception(256, 128, (128, 192), (32, 96), 64),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        )
+        self.block4 = nn.Sequential(
+            Inception(480, 192, (96, 208), (16, 48), 64),
+            Inception(512, 160, (112, 224), (24, 64), 64),
+            Inception(512, 128, (128, 256), (24, 64), 64),
+            Inception(512, 112, (144, 288), (32, 64), 64),
+            Inception(528, 256, (160, 320), (32, 128), 128),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        )
+        self.block5 = nn.Sequential(
+            Inception(832, 256, (160, 320), (32, 128), 128),
+            Inception(832, 384, (192, 384), (48, 128), 128),
+            nn.AdaptiveAvgPool2d((1, 1)),
+            nn.Flatten(),
+            nn.Linear(1024, 1000)
+        )
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+
+                elif isinstance(m, nn.Linear):
+                    nn.init.normal_(m.weight, 0, 0.01)
+                    if m.bias is not None:
+                        nn.init.constant_(m.bias, 0)
+
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.block3(x)
+        x = self.block4(x)
+        x = self.block5(x)
+        return x
+
+if __name__ == '__main__':
+    model = GoogLeNet(Inception)
+    model = model.cuda()
+    summary(model, (3, 224, 224), device='cuda')
